865d4e529f7bab96481609b13a4a8cebf1ceaf2c
KrishnaWosti75 <wostikrishna58@gmail.com>
2025-03-27 14:52:55 +0545

    added sanity cli .env

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -2,10 +2,11 @@ import { createClient } from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
 export const sanityClient = createClient({
-    projectId: '5b16yo6s',
-    dataset: 'production',
-    useCdn: true,
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+    useCdn: false,
     apiVersion: '2024-02-27',
+    token: process.env.SANITY_API_TOKEN,
 })
 
 const builder = imageUrlBuilder(sanityClient)
